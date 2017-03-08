1a356bdb0d36d0361707a0bd734594e8ce5efc90
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-03-08 14:46:47 -0800

    adding newest version of game

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # TriviaGame
-Homework Week 5
+#Homework Week 5
+
+#Live Link 
+* https://nicothegeeko.github.io/TriviaGame
+
+Try to answer as many trivia questions as possible in the time allowed. 
+
+#Requirements
+* Create a trivia form with multiple choice or true/false options (your choice).
+The player will have a limited amount of time to finish the quiz.
+The game ends when the time runs out. The page will reveal the number of questions that players answer correctly and incorrectly.
+Don't let the player pick more than one answer per question.
+Don't forget to include a countdown timer.
+Option Two: Advanced Assignment (Timed Questions)
+
+*Advanced
+
+You'll create a trivia game that shows only one question until the player answers it or their time runs out.
+If the player selects the correct answer, show a screen congratulating them for choosing the right option. After a few seconds, display the next question -- do this without user input.
+The scenario is similar for wrong answers and time-outs.
+If the player runs out of time, tell the player that time's up and display the correct answer. Wait a few seconds, then show the next question.
+If the player chooses the wrong answer, tell the player they selected the wrong option and then display the correct answer. Wait a few seconds, then show the next question.
+On the final screen, show the number of correct answers, incorrect answers, and an option to restart the game (without reloading the page).
+
+
+Technologies Used
+
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* Jquery
+
+Code Explaination
+*Attempted to make a game in which the user attempts to answer questions with only a certain amount of time to correctly answer the quiz. 
---
+++ assets/javascript/app.js
@@ -0,0 +1,177 @@
+$(document).ready(function() {
+
+    var correctAnswer;
+    var questions;
+    var wrongAnswers;
+    var timer;
+    var incorrectAnswer;
+    var setInvervalId;
+    var wonGame;
+    var quest = 0;
+
+    //start the game by clicking on the button
+
+    function startScreen() {
+        $(".start-button").click(function() {
+            $(this).hide(2000, function() {
+                $(this).remove();
+            });
+        });
+
+    }
+
+    startScreen();
+
+    $("body").on("click", ".start-button", function(event) {
+        generateHTML();
+        startTimer();
+
+
+    });
+
+    //generating HTML for questions and answers
+
+    function generateHTML() {
+        gameHTML =
+            "<p class='text-center'>" + questionsAndAnswers[quest].question + "</p>";
+        $(".mainSection").html(gameHTML);
+        $("#time-left").text("Time Left: ");
+
+        for (var i = 0; i < questionsAndAnswers[quest].answer.length; i++) {
+
+            var button = $("<button>");
+
+            button.text(questionsAndAnswers[quest].answer[i]);
+
+            $("#choices-div").append(button);
+
+        }
+
+
+
+    }
+
+    //create an event for when the correct/wrong answer is clicked 
+
+
+    $('#choices-div').on('click', 'button', function(e) {
+        userPick = $(this).html();
+
+
+        if (userPick != correctAnswers[quest]) {
+            $('#choices-div').html("Wrong Answer!");
+
+
+            incorrectAnswer++;
+            quest++;
+            generateHTML();
+
+        } else if (userPick === correctAnswers[quest]) {
+            $('#choices-div').html("Correct!");
+            correctAnswer++;
+            quest++;
+            generateHTML();
+        }
+    });
+
+    function display() {
+
+    }
+
+
+
+
+
+
+    //Timer Controls
+
+    function startTimer() {
+
+        var timeLeft = 30;
+        var elem = document.getElementById('display');
+
+        var setIntervalId = setInterval(countdown, 1000);
+
+        function countdown() {
+            if (timeLeft === -1) {
+                clearInterval(setIntervalId);
+                setIntervalId = setInterval(countdown, 1000);
+                var noTime = $('#start').html("Sorry, you ran out of Time");
+                setTimeout(function() {
+                    noTime.text("");
+                }, 2000);
+
+
+
+                clearTimeout(setIntervalId);
+            } else {
+                elem.innerHTML = timeLeft + ' Seconds Remaining';
+                timeLeft--;
+
+
+
+            }
+        }
+    }
+
+
+    //trying to keep score of correct and wrong answers
+    function wonGame() {
+
+        if (correctAnswer < 3) {
+            $('#choices-div').text("You Lost!");
+        } else if (correctAnswer == 3) {
+            $('#choices-div').text("You Won");
+        }
+    };
+
+
+    //restart the game 
+    function restartGame() {
+        document.location.reload();
+    }
+
+    //questions for the games 
+    var questionsAndAnswers = [{
+            question: "Why was the Doctor's companion, Clara's, leaf so important?",
+            answer: [
+                "It was her key to a new world",
+                "It was how her parents met",
+                "It represented her and the Doctor's relationship",
+                "It was the last leaf found on Gallifrey"
+            ]
+        }, {
+            question: "Who was the 10th Doctor?",
+            answer: [
+                "Matt Smith",
+                "David Tennant",
+                "Christopher Eccleston",
+                "Rose Tyler"
+            ]
+        },
+
+        {
+            question: "How long has doctor who been on the air?",
+            answer: [
+                "38 Years",
+                "50 Years",
+                "He's a Time Lord so forever",
+                "12 Years"
+            ]
+        }
+
+    ];
+
+
+    //correct answers for the game 
+    var correctAnswers = ["It was how her parents met", "David Tennant", "38 Years"];
+
+
+
+
+
+
+
+    var gameHTML;
+
+});
